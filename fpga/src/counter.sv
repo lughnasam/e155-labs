99b80e96d571b6705ecec880efcbf77c9845172c
lughnasay <meande04@gmail.com>
2025-09-04 14:12:09 -0700

    added comments, headers

--- fpga/src/counter.sv
+++ fpga/src/counter.sv
@@ -1,3 +1,10 @@
+/*
+Name: Lughnasa Miller
+Date: 9/3/25
+Email: lumiller@hmc.edu
+Description: This module implements a basic counter with a variable bus width and 
+    incrementing value (default 24 and 1, respectively).
+*/
 module counter 
 	#(parameter WIDTH = 24,
 	  parameter P = 1) 
