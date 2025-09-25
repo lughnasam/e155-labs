8ec2cf261f05f137c6fb15a3ff1e709ebc16bd7f
Lughnasa Miller <lumiller@hmc.edu>
2025-09-25 01:20:18 -0700

    flashed and it didnt work

---
+++ lab3/fpga/src/button_decoder.sv
@@ -0,0 +1,33 @@
+module button_decoder(input  [3:0] rows, cols,
+                      output [6:0] seg);
+
+logic [7:0] keypad;
+logic [3:0] number;
+
+assign keypad = {rows, cols};
+
+always_comb begin
+    case(keypad)
+        8'b1000_0001: number = 4'b0001;
+        8'b1000_0010: number = 4'b0010;
+        8'b1000_0100: number = 4'b0011;
+        8'b1000_1000: number = 4'b1100;
+        8'b0100_0001: number = 4'b0100;
+        8'b0100_0010: number = 4'b0101;
+        8'b0100_0100: number = 4'b0110;
+        8'b0100_1000: number = 4'b1101;
+        8'b0010_0001: number = 4'b0111;
+        8'b0010_0010: number = 4'b1000;
+        8'b0010_0100: number = 4'b1001;
+        8'b0010_1000: number = 4'b1110;
+        8'b0001_0001: number = 4'b1010;
+        8'b0001_0010: number = 4'b0000;
+        8'b0001_0100: number = 4'b1011;
+        8'b0001_1000: number = 4'b1111;
+        default:      number = 4'b0000;
+    endcase
+end
+
+seven_seg_decoder ssd(.s(number), .seg(seg));
+
+endmodule
--- lab3/fpga/src/lab3_lm.sv
+++ lab3/fpga/src/lab3_lm.sv
@@ -4,14 +4,14 @@ module lab3_lm(input logic reset,
                output logic [6:0] segout,
                output logic lpwr, rpwr);
 
-    logic        clk;
-    logic [3:0]  cur_press, cur_in, cur_num, prev_num;
+    logic        clk, new_key;
+    logic [3:0]  cols_nk;
+	logic [6:0]  cur_in, cur_num, prev_num;
+    logic [7:0]  new_num;
     logic [17:0] c_val;
 
-    
-    
-    
-
+    // fsm and button handler
+    input_fsm scanner(.clk(clk), .reset(reset), .cols(cols), .new_key(new_key), .row_pwr(rows), .cols_newkey(cols_nk));
 
     // create clock and timing
     HSOSC #(.CLKHF_DIV(2'b01)) hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk));
@@ -21,9 +21,12 @@ module lab3_lm(input logic reset,
     assign l_seg_pwr = c_val[17];
     assign r_seg_pwr = ~c_val[17];
 
+    // decode button into seg
+    button_decoder bd(.rows(rows), .cols(cols_nk), .seg(cur_in));
+
     // shift register setup to store previous two outputs
-    ff_enabled #(.WIDTH(4)) ff_r(.clk(clk), .reset(reset), .enable(new_key), .d(cur_in), .q(cur_num));
-    ff_enabled #(.WIDTH(4)) ff_l(.clk(clk), .reset(reset), .enable(new_key), .d(cur_num), .q(prev_num));
+    ff_enabled #(.WIDTH(7)) ff_r(.clk(clk), .reset(reset), .enable(new_key), .d(cur_in), .q(cur_num));
+    ff_enabled #(.WIDTH(7)) ff_l(.clk(clk), .reset(reset), .enable(new_key), .d(cur_num), .q(prev_num));
 
     // when ctr[17] == 1, power and output left side, else power and output right
     assign lpwr = c_val[17];
