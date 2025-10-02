08a670ad51e3dbbe913bff2255759be79ba19776
Lughnasa Miller <lumiller@hmc.edu>
2025-10-01 22:31:14 -0700

    lab 3 progress?

--- lab3/fpga/src/button_decoder.sv
+++ lab3/fpga/src/button_decoder.sv
@@ -8,22 +8,22 @@ assign keypad = {rows, cols};
 
 always_comb begin
     case(keypad)
-        8'b1000_0001: number = 4'b0001;
-        8'b1000_0010: number = 4'b0010;
-        8'b1000_0100: number = 4'b0011;
-        8'b1000_1000: number = 4'b1100;
-        8'b0100_0001: number = 4'b0100;
-        8'b0100_0010: number = 4'b0101;
-        8'b0100_0100: number = 4'b0110;
-        8'b0100_1000: number = 4'b1101;
-        8'b0010_0001: number = 4'b0111;
-        8'b0010_0010: number = 4'b1000;
-        8'b0010_0100: number = 4'b1001;
-        8'b0010_1000: number = 4'b1110;
-        8'b0001_0001: number = 4'b1010;
-        8'b0001_0010: number = 4'b0000;
-        8'b0001_0100: number = 4'b1011;
-        8'b0001_1000: number = 4'b1111;
+        8'b1000_0001: number = 4'b1110;
+        8'b1000_0010: number = 4'b0000;
+        8'b1000_0100: number = 4'b1111; 
+        8'b1000_1000: number = 4'b1101;
+        8'b0100_0001: number = 4'b0111;
+        8'b0100_0010: number = 4'b1000;
+        8'b0100_0100: number = 4'b1001;
+        8'b0100_1000: number = 4'b1100;
+        8'b0010_0001: number = 4'b0100;
+        8'b0010_0010: number = 4'b0101;
+        8'b0010_0100: number = 4'b0110;
+        8'b0010_1000: number = 4'b1011;
+        8'b0001_0001: number = 4'b0001;
+        8'b0001_0010: number = 4'b0010;
+        8'b0001_0100: number = 4'b0011;
+        8'b0001_1000: number = 4'b1010;
         default:      number = 4'b0000;
     endcase
 end
--- lab3/fpga/src/lab3_lm.sv
+++ lab3/fpga/src/lab3_lm.sv
@@ -2,27 +2,28 @@ module lab3_lm(input logic reset,
                input logic [3:0] cols, 
                output logic [3:0] rows,
                output logic [6:0] segout,
-               output logic lpwr, rpwr);
+               output logic lpwr, rpwr,
+               output logic [3:0] stateout);
 
     logic        clk, new_key;
-    logic [3:0]  cols_nk;
+    logic [3:0]  rows_synced, cols_synced;
 	logic [6:0]  cur_in, cur_num, prev_num;
     logic [7:0]  new_num;
     logic [17:0] c_val;
 
     // fsm and button handler
-    input_fsm scanner(.clk(clk), .reset(reset), .cols(cols), .new_key(new_key), .row_pwr(rows), .cols_newkey(cols_nk));
+    input_fsm scanner(.clk(clk), .reset(reset), .cols(cols), .new_key(new_key), .row_pwr(rows), .stateout(stateout));
+
+    // synchronizers
+    synchronizer #(.WIDTH(4)) rows_sync(.clk(clk), .reset(reset), .d(rows), .q(rows_synced));
+    synchronizer #(.WIDTH(4)) cols_sync(.clk(clk), .reset(reset), .d(cols), .q(cols_synced));
 
     // create clock and timing
     HSOSC #(.CLKHF_DIV(2'b01)) hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk));
     counter #(.WIDTH(18))      count(.clk(clk), .reset(reset), .value(c_val));
-    
-    // power state determined by ctr
-    assign l_seg_pwr = c_val[17];
-    assign r_seg_pwr = ~c_val[17];
 
     // decode button into seg
-    button_decoder bd(.rows(rows), .cols(cols_nk), .seg(cur_in));
+    button_decoder bd(.rows(rows_synced), .cols(cols_synced), .seg(cur_in));
 
     // shift register setup to store previous two outputs
     ff_enabled #(.WIDTH(7)) ff_r(.clk(clk), .reset(reset), .enable(new_key), .d(cur_in), .q(cur_num));
